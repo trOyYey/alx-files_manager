e9e0e18b999b56e4680bdf2f03fad37b59c7086f
trOyYey <troyzuwara@gmail.com>
2024-12-13 20:07:38 +0000

    0-redis utils

---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(error.message);
+    });
+  }
+
+  isAlive() {
+    if (this.client.ready) {
+      return true;
+    }
+    return false;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
+module.exports = redisClient;
